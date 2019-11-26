64a66f05911951c93eed69763c97267c565e0642
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-26 06:16:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,5 +134,5 @@ def adding_to_dennis
         }
      }
      programmer_hash[:alan_kay][:languages] << "Assembly"
-     programmer_hash[:alan_kay]
+     programmer_hash[:alan_kay][:languages]
 end 
