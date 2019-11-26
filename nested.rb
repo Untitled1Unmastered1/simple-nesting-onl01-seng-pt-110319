3ba4d591b453f69dcad174e5a0627e043a49978f
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-26 06:17:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,6 +133,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:languages] << "Assembly"
-     programmer_hash[:alan_kay][:languages][1]
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end 
